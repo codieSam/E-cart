ca7cba2ae684a9a51fe42fc2546c6c4afe76789b
Samrat Belbase <samratbelbase408@gmail.com>
2025-04-22 23:27:59 +0545

    Bottom banner section added successfully

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import MainBanner from "../components/MainBanner";
 import Category from "../components/Category";
 import BestSeller from "../components/BestSeller";
+import BottomBanner from "../components/BottomBanner";
 
 const Home = () => {
   return (
@@ -9,6 +10,7 @@ const Home = () => {
       <MainBanner />
       <Category />
       <BestSeller />
+      <BottomBanner />
     </div>
   );
 };
